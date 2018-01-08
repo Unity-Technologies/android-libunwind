e6686501ef771e4851c733377f3c8cd1937c0cd5
Ignas Ziberkas <ignas@unity3d.com>
2018-01-08 19:01:27 +0200

    Add build script.

---
+++ unity/build.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use File::Basename qw(dirname);
+use File::Copy qw(move);
+use File::Path qw(mkpath rmtree);
+use File::Spec qw(rel2abs);
+use lib File::Spec->rel2abs(dirname(__FILE__)) . '/perl';
+use PrepareAndroidSDK;
+
+PrepareAndroidSDK::GetAndroidSDK("android-21", undef, "r13b");
+
+die("ANDROID_NDK_ROOT environment variable is not set") unless $ENV{ANDROID_NDK_ROOT};
+chdir(File::Spec->rel2abs(dirname(__FILE__)));
+
+if (-d 'builds')
+{
+	rmtree('builds') or die('Failed to delete builds directory');
+}
+
+if (-d 'lib')
+{
+	rmtree('lib') or die('Failed to delete lib directory');
+}
+
+if (-d 'obj')
+{
+	rmtree('obj') or die('Failed to delete obj directory');
+}
+
+my $ndkBuild = $ENV{ANDROID_NDK_ROOT} . '/ndk-build';
+my $ndkDebug = 0;
+system($ndkBuild, "NDK_DEBUG=$ndkDebug", 'clean');
+system($ndkBuild, "NDK_DEBUG=$ndkDebug");
+
+mkpath('builds/arm64-v8a') or die('Failed to create builds/arm64-v8a directory');
+move('obj/local/arm64-v8a/libunwind.a', 'builds/arm64-v8a/libunwind.a') or die('Failed to move arm64-v8a/libunwind.a');
+File::Copy::Recursive::dircopy('../include', 'builds/include') or die ('Failed to copy include directory');
